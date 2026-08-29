088f526bfe1271eba962a6b6e3de0c68a6b7eb93
vtu27514 <vtu27514@veltech.edu.in>
2026-08-29 08:24:39 +0530

    Implement mergeTwoLists method for sorted lists
    Implemented a method to merge two sorted linked lists into one sorted linked list. Added a main method to demonstrate the functionality with example lists.

---
+++ Task13/MergeTwoSortedLists.java
@@ -0,0 +1,56 @@
+public class MergeTwoSortedLists {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode result = mergeTwoLists(list1, list2);
+
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
