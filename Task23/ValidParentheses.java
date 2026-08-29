93e8083ebca4aad56c19c073ec21bddcaa85b37d
vtu27514 <vtu27514@veltech.edu.in>
2026-08-29 08:29:43 +0530

    Create ValidParentheses.java

---
+++ Task23/ValidParentheses.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        String s = "()[]{}";
+
+        System.out.println(isValid(s));
+    }
+}
