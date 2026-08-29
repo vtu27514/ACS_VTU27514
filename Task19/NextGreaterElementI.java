c3b571b89cc9438d39d1689ba906ee2eebb78e9d
vtu27514 <vtu27514@veltech.edu.in>
2026-08-29 08:27:41 +0530

    Create NextGreaterElementI.java

---
+++ Task19/NextGreaterElementI.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.Stack;
+import java.util.Arrays;
+
+public class NextGreaterElementI {
+
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                map.put(nums2[i], -1);
+            } else {
+                map.put(nums2[i], stack.peek());
+            }
+
+            stack.push(nums2[i]);
+        }
+
+        int[] result = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = map.get(nums1[i]);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
+
+        int[] result = nextGreaterElement(nums1, nums2);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
