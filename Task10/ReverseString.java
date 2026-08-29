0bcaff91652334ef2eea2e9b68033f884de368c2
vtu27514 <vtu27514@veltech.edu.in>
2026-08-29 08:22:31 +0530

    Implement string reversal in ReverseString class

---
+++ Task10/ReverseString.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class ReverseString {
+
+    public static void reverseString(char[] s) {
+
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+
+        reverseString(s);
+
+        System.out.println(Arrays.toString(s));
+    }
+}
