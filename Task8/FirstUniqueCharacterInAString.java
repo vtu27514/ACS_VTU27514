1dad279e501e9411513a759fb154b6dab0f42ccc
vtu27514 <vtu27514@veltech.edu.in>
2026-08-29 08:21:15 +0530

    Implement first unique character finder in string

---
+++ Task8/FirstUniqueCharacterInAString.java
@@ -0,0 +1,28 @@
+public class FirstUniqueCharacterInAString {
+
+    public static int firstUniqChar(String s) {
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (count[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "leetcode";
+
+        int result = firstUniqChar(s);
+
+        System.out.println("First unique character index: " + result);
+    }
+}
