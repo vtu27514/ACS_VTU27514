53002393f4208ec2160ee842a4e78359ea2ddf2a
vtu27514 <vtu27514@veltech.edu.in>
2026-09-10 09:17:15 +0530

    Create BinaryTreeLevelOrder.java

---
+++ task 31-binary tree level order/BinaryTreeLevelOrder.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class BinaryTreeLevelOrder{
+
+    // Tree Node
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode current = queue.poll();
+
+                level.add(current.val);
+
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+
+            result.add(level);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        /*
+                 3
+                / \
+               9   20
+                  /  \
+                 15   7
+        */
+
+        TreeNode root = new TreeNode(3);
+
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        List<List<Integer>> result = levelOrder(root);
+
+        System.out.println(result);
+    }
+}
